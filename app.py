705d6539e6073a6a4f64dda5c40ce0e7e1e08daa
AdminTima <tima.khilkevich@inbox.ru>
2022-12-14 09:58:07 +0300

    Added routes dictionary. Added route method that wraps request handlers and register them and appropriate routes to the routes dict.Added method that handles case when route not found.

--- app.py
+++ app.py
@@ -4,5 +4,15 @@ from tpw.core import Tpw
 app = Tpw()
 
 
+@app.route("/")
+def home(request):
+    return "Hello home page"
+
+
+@app.route("/about")
+def about(request):
+    return "Hello about page"
+
+
 if __name__ == "__main__":
     serve(app)
--- tpw/core.py
+++ tpw/core.py
@@ -1,9 +1,41 @@
+from webob import Request, Response
 
 
 class Tpw:
 
-    def __call__(self, eniron, start_response):
-        status = "200 OK"
-        resp = b"Hello world class"
-        start_response(status, headers=[])
-        yield resp
+    def __init__(self):
+        self.__routes = {}
+
+    def __call__(self, environ, start_response):
+        request = Request(environ)
+        response = self.handle_request(request)
+        return response(environ, start_response)
+
+    def handle_request(self, request):
+        response = Response()
+        handler = self.find_handler(request.path)
+
+        if handler is None:
+            return self.not_found_response(response)
+
+        response.text = handler(request)
+        return response
+
+    def find_handler(self, request_path):
+        for path, handler in self.__routes.items():
+            if path == request_path:
+                return handler
+
+    def route(self, path):
+        def wrapper(handler):
+            self.__routes[path] = handler
+            print(self.__routes)
+            return handler
+        return wrapper
+
+    def not_found_response(self, response):
+        response.text = "Not found page"
+        response.status_code = 404
+        return response
+
+
